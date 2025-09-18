76b963836d00654728efeffb00f95870bb20509d
Adam Bradley <20134571@tafe.wa.edu.au>
2025-09-18 11:54:54 +0800

    Fixed person constructor again

--- src/main.py
+++ src/main.py
@@ -6,7 +6,15 @@ person=ur.Person()
 young=ur.Person("Michael Hepburn")
 person.addFriend(young)
 
-person3=ur.Person("Other Michael")
+person3=ur.Person("Michael OBrien")
 person.addFriend(person3)
+
+person4=ur.Person("Ian McMullen")
+person.addFriend(person4)
+
+person5=ur.Person("Kaltails David Honey")
+person.addFriend(person5)
+
+
 
 print(person.friends)
